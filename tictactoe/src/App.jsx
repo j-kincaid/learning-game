400db09390311b5fa4f0b575534b9019d7610b1d
Jessica Kincaid <4j.kincaid@gmail.com>
2024-12-03 18:32:02 -0600

    reduce to minimum to bring in props

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+// Jessica Kincaid December 4, 2024
+// Geography Flashcards for WEB 234 - Web Apps I 
+
+// Working from class example
+
+// Change the css in App.css, not
+
 function Display({ title, children }) {
   const[isActive, setIsActive] = useState(false);
   return (
@@ -13,75 +20,48 @@ function Display({ title, children }) {
     </section>
   );
 }
+
+// This needs some props for the flashcards in the form of a hard-coded list of objects or imported from an API.
+
 function Cards() {
   return (
     <>
     {/* This would be helpful for an FAQ section */}
+
+<p>Guess the capital city of each country.</p>
       {/* <h2>Geography</h2> */}
+
       <Display title=" Botswana">
       Gaborone
       </Display>
-
+        <div>
       <Display title="New Zealand">
-        Wellington
+
+
+      <h3 class="cap">
+      Wellington</h3>
+      </Display>
+      </div>
+
+
+      <Display title="Chile">
+        Santiago
+      </Display>
+
+      <Display title="Bangladesh">
+        Dhaka
       </Display>
     </>
   );
 
-}function Characters2() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  return (
-    <>
-      {/* <h2>Spongebob</h2> */}
-      <Display2
-        title="The Characters"
-        isActive={activeIndex === 0}
-        onShow={() => setActiveIndex(0)}
-      >
-        Spongebob Patrick Sandy
-      </Display2>
-      <Display2
-        title="Where They Live"
-        isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(1)}
-      >
-        Pineapple Rock Anchor
-      </Display2>
-    </>
-  );
 }
 
-function Display2({ title, children, isActive, onShow}) {
-  return (
-    <section className="Display2">
-      <h3>{title}</h3>
-      {isActive ? (
-        <p>{children}</p>
-      ) : (
-        <button onClick={onShow}>
-          Show
-        </button>
-      )}
-    </section>
-  );
-}
 
 
 export default function App() {
   return(
   <>
-<table>
-<tr>
-<th>Geography</th>
-  <Cards />
-</tr>
-  <h2> -------------</h2>
-  <tr>
-    
-  <th>Celebrities</th>
-  <Characters2 />
-  </tr>
-</table>
+<Cards />
 
   </>
   );
