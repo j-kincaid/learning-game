6e3884f0c3b2873970cd54e5867321e571b0ef76
Jessica Kincaid <4j.kincaid@gmail.com>
2024-12-04 13:47:32 -0600

    make 4 cards with images and an ugly little artifact, still need to style the cards

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,68 +1,131 @@
 import { useState } from 'react';
+import './App.css'
+
 
 // Jessica Kincaid December 4, 2024
 // Geography Flashcards for WEB 234 - Web Apps I 
 
-// Working from class example
+// Change the css in App.css
 
-// Change the css in App.css, not
 
 function Display({ title, children }) {
   const[isActive, setIsActive] = useState(false);
   return (
     <section className="display">
-      <h3>{title}</h3>
+      <h4>{title}</h4>
       {isActive ? ( <p>{children}</p> ) : (
         <button onClick={() => setIsActive(true)}>
-          Show
+          Reveal Country
         </button>
       )}
     </section>
   );
 }
 
-// This needs some props for the flashcards in the form of a hard-coded list of objects or imported from an API.
 
-function Cards() {
+
+function Bangladesh() {
   return (
-    <>
-    {/* This would be helpful for an FAQ section */}
-
-<p>Guess the capital city of each country.</p>
-      {/* <h2>Geography</h2> */}
-
-      <Display title=" Botswana">
-      Gaborone
-      </Display>
-        <div>
-      <Display title="New Zealand">
-
-
-      <h3 class="cap">
-      Wellington</h3>
-      </Display>
-      </div>
-
-
-      <Display title="Chile">
-        Santiago
-      </Display>
-
-      <Display title="Bangladesh">
-        Dhaka
-      </Display>
-    </>
+    <img
+    country="Bangladesh"
+    src="https://imgur.com/aFWnMQSs"
+    />
   );
-
 }
 
+function Turkmenistan() {
+  return (
+    <img
+    country="Turkmenistan"
+    src="https://imgur.com/6QkYTkXs"
+    />
+  );
+}
+
+function Malaysia() {
+  return (
+    <img
+    country="Malaysia"
+    src="https://imgur.com/ViRKEIZs"
+    />
+  );
+}
+
+function Estonia() {
+  return (
+    <img
+    country="Estonia"
+    src="https://imgur.com/zo04Xkks"
+    />
+  );
+}
+
+function FlashCardPic({photo}) {
+  return (
+    <>
+      <img src={getUrl(photo)} />
+    </>
+  )
+}
+
+function getUrl(photo) {
+  return (
+    `https://imgur.com/${photo.id}.jpeg`
+  );
+}
 
 
 export default function App() {
   return(
   <>
-<Cards />
+  <h4>Below are scenes from capital cities around the world. </h4>
+<p>Guess the country that goes with each capital city!</p>
 
+{/* // Dhaka, Bangladesh https://imgur.com/aFWnMQS  */}
+
+<Bangladesh />
+      <FlashCardPic
+        photo={{ 
+        id: 'aFWnMQSm'}}
+        />
+<Display title=" Dhaka">
+     Bangladesh
+      </Display>
+
+
+
+<Estonia />
+      <FlashCardPic
+        photo={{ 
+        id: 'zo04Xkkm'}}
+        />
+<Display title=" Tallinn">
+     Estonia
+      </Display>
+
+
+
+ <Turkmenistan />
+      <FlashCardPic
+        photo={{ 
+        id: '6QkYTkXm'}}
+        />
+
+<Display title=" Ashgabat">
+      Turkmenistan
+      </Display>
+
+
+
+  <Malaysia />
+      <FlashCardPic
+        photo={{ 
+        id: 'ViRKEIZm'}}
+        />
+
+<Display title=" Kuala Lumpur">
+      Malaysia
+      </Display>
   </>
   );
   }
